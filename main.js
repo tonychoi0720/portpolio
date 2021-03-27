c7146e997cf10b7c09a121374d21c2eea44f068f
tonychoi0720 <80289454+tonychoi0720@users.noreply.github.com>
2021-03-27 10:39:37 +0900

    JS: Home contact buttton

--- main.js
+++ main.js
@@ -5,14 +5,14 @@
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
-    if(window.scrollY > navbarHeight) {
+    if (window.scrollY > navbarHeight) {
         navbar.classList.add('navbar-dark');
     } else {
         navbar.classList.remove('navbar-dark');
     }
 });
 
-// Handle scrolling when tapping on the navbar meny
+// Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
     const target = event.target;
@@ -21,7 +21,16 @@ navbarMenu.addEventListener('click', (event) => {
         return;
     }
 
-    console.log(event.target.dataset.link);
-    const scrollTo = document.querySelector(link)
-    scrollTo.scrollIntoView({behavior: "smooth"});
-})
+    scrollIntoView(link);
+});
+
+// Handle scrolling when click on the button
+const homeBtn = document.querySelector('.home__contact');
+homeBtn.addEventListener('click', () => {
+    scrollIntoView('#contact');
+});
+
+function scrollIntoView(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior:"smooth"});
+}
