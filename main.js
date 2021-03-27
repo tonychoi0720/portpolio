b9679d9cf2d920b632a6b561fbcc7645d2ff7f05
tonychoi0720 <80289454+tonychoi0720@users.noreply.github.com>
2021-03-27 11:08:15 +0900

    JS: transparent

--- main.js
+++ main.js
@@ -34,3 +34,11 @@ function scrollIntoView(selector) {
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({behavior:"smooth"});
 }
+
+
+// make opacity when scrolling down the page
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+    home.style.opacity = 1 - window.scrollY / homeHeight;
+});
