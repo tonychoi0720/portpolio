5c50d97c8bb40d673dc04038e77978e9a8bbbf9a
tonychoi0720 <80289454+tonychoi0720@users.noreply.github.com>
2021-03-27 11:37:54 +0900

    JS: arrow up

--- main.js
+++ main.js
@@ -30,15 +30,29 @@ homeBtn.addEventListener('click', () => {
     scrollIntoView('#contact');
 });
 
-function scrollIntoView(selector) {
-    const scrollTo = document.querySelector(selector);
-    scrollTo.scrollIntoView({behavior:"smooth"});
-}
-
-
 // make opacity when scrolling down the page
 const home = document.querySelector('.home__container');
 const homeHeight = home.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
     home.style.opacity = 1 - window.scrollY / homeHeight;
 });
+
+// Show "arrow-up" button when scrolling down
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+    if(window.scrollY > homeHeight / 2) {
+        arrowUp.classList.add('visible');
+    } else {
+        arrowUp.classList.remove('visible');
+    }
+});
+
+arrowUp.addEventListener('click', () => {
+    scrollIntoView('#home');
+});
+
+// Function
+function scrollIntoView(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior:"smooth"});
+}
